e6df9f4b0ac81f65a2629cd2be5e6e3051959fc1
karun777 <karun777@outlook.com>
2020-04-19 22:34:11 +0545

    while outside click dropdown shuts off

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const NavBar = (props) => {
@@ -11,9 +11,24 @@ export const NavBar = (props) => {
 
 export const NavItem = (props) => {
   const [open, setOpen] = useState(false);
+  const node = useRef();
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+  const handleClick = (e) => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
-    <li className="nav-item">
+    <li ref={node} className="nav-item">
       <Link to="/">
         <div className="icon-button" onClick={() => setOpen(!open)}>
           {props.icon}
